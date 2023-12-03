2b6e285751d9dec501d2262a35afb14df90d7779
Joao Vitor <jvitor2000@outlook.com>
2023-12-03 14:15:10 -0300

    feature: Utiliza a interface de click nos TodoListAdapter

--- app/src/main/java/com/example/todolist/adapters/TodoListAdapter.java
+++ app/src/main/java/com/example/todolist/adapters/TodoListAdapter.java
@@ -10,19 +10,30 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.todolist.R;
+import com.example.todolist.interfaces.IOnCardClickListener;
 import com.example.todolist.models.TodoList;
 
 import java.util.List;
 
 public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.TodoListViewHolder> {
     private final List<TodoList> todoLists;
+    private IOnCardClickListener listener;
+
+    public void setOnCardClickListener(IOnCardClickListener listener) {
+        this.listener = listener;
+    }
     public static class TodoListViewHolder extends RecyclerView.ViewHolder {
         public TextView listTitle;
         public TextView listDescription;
-        public TodoListViewHolder(@NonNull View v) {
+        public TodoListViewHolder(@NonNull View v, final IOnCardClickListener listener) {
             super(v);
             this.listTitle = v.findViewById(R.id.list_title);
             this.listDescription = v.findViewById(R.id.list_description);
+            v.setOnClickListener(c -> {
+                if (listener != null && getAdapterPosition() != RecyclerView.NO_POSITION) {
+                    listener.onCardClick(getAdapterPosition());
+                }
+            });
         }
     }
 
@@ -34,7 +45,7 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.TodoLi
     @Override
     public TodoListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_card, parent, false);
-        return new TodoListViewHolder(v);
+        return new TodoListViewHolder(v, this.listener);
     }
 
     @Override
