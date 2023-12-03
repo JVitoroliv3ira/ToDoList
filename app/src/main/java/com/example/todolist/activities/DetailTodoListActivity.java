fa4571bfd4074de363555750d2fb7e26dc158ff7
Joao Vitor <jvitor2000@outlook.com>
2023-12-03 14:48:07 -0300

    feature: Buscar a TodoList selecionada

--- app/src/main/java/com/example/todolist/activities/DetailTodoListActivity.java
+++ app/src/main/java/com/example/todolist/activities/DetailTodoListActivity.java
@@ -1,16 +1,24 @@
 package com.example.todolist.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.todolist.R;
+import com.example.todolist.models.TodoList;
+import com.example.todolist.readers.TodoListReader;
 
 public class DetailTodoListActivity extends AppCompatActivity {
+    private TodoListReader todoListReader;
+
+    private TodoList list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_todo_list);
+
+        this.todoListReader = new TodoListReader(this.getApplicationContext());
+        this.list = this.todoListReader.getEntityById(this.getIntent().getIntExtra("listId", 0));
     }
 }
